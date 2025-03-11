9f2f67a647dff9206fa12f08fd56f606d9513979
di <dit99871@gmail.com>
2025-03-11 19:52:16 +0400

    Create crud/user

---
+++ src/backend/crud/users.py
@@ -0,0 +1,25 @@
+from typing import Sequence
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import User
+from core.schemas.user import UserCreate
+
+
+async def get_user(
+    session: AsyncSession,
+    user_id: int,
+) -> User | None:
+    return await session.get(User, user_id)
+
+
+async def create_user(
+    session: AsyncSession,
+    user_create: UserCreate,
+) -> User:
+    user = User(**user_create.model_dump())
+    session.add(user)
+    await session.commit()
+    # await session.refresh(user)
+    return user
