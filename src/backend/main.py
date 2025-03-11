c8b451e324b98d17b641930dfec749bd139c79cc
di <dit99871@gmail.com>
2025-03-11 19:52:59 +0400

    Add logging in main

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,14 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
+
+logging.basicConfig(
+    level=settings.logging.log_level_value,
+    format=settings.logging.log_format,
+)
 
 app = FastAPI()
 
@@ -12,4 +19,9 @@ def main():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=settings.run.host, port=settings.run.port)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
