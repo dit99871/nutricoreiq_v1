a5c51bd021c0f45e7c5445da384005eefc0ad2ed
di <dit99871@gmail.com>
2025-03-11 19:51:17 +0400

    Create model user

---
+++ src/backend/core/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = (
+    "db_helper",
+    "Base",
+    "Product",
+    "User",
+)
+
+from .db_helper import db_helper
+from .base import Base
+from .user import User
+from .product import Product
