c419bc0ccaf3b925910139ee2c61af0a04cebe2c
di <dit99871@gmail.com>
2025-03-13 18:17:11 +0400

    Add gender_age_specific_nutrient model

---
+++ backend/src/app/core/models/gender_age_specific_nutrient.py
@@ -0,0 +1,22 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+
+class GenderAgeSpecificNutrient(IntIdPkMixin, Base):
+    age_group_id: Mapped[int] = mapped_column(ForeignKey("age_groups.id"))
+    nutrient_id: Mapped[int] = mapped_column(ForeignKey("nutrients.id"))
+    demand: Mapped[float] = mapped_column(nullable=False)
+
+    age_group = relationship("AgeGroup", back_populates="nutrients")
+    nutrient = relationship("Nutrient")
+
+
+class MaleAgeSpecificNutrient(GenderAgeSpecificNutrient):
+    pass
+
+
+class FemaleAgeSpecificNutrient(GenderAgeSpecificNutrient):
+    pass
