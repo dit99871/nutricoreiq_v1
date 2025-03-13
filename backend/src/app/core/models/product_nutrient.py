e14d2e335aa2035ba0ebdc9baa5fc79589c4c77b
di <dit99871@gmail.com>
2025-03-13 18:23:36 +0400

    Add product_nutrient model

---
+++ backend/src/app/core/models/product_nutrient.py
@@ -0,0 +1,17 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+
+class ProductNutrient(IntIdPkMixin, Base):
+    amount: Mapped[float] = mapped_column(nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), primary_key=True)
+    nutrient_id: Mapped[int] = mapped_column(
+        ForeignKey("nutrients.id"),
+        primary_key=True,
+    )
+
+    product = relationship("Product", back_populates="nutrients")
+    nutrient = relationship("Nutrient", back_populates="products")
