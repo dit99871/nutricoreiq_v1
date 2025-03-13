1349638e1f97e5bf182ed8f49d88e7d9331951cc
di <dit99871@gmail.com>
2025-03-13 07:56:43 +0400

    Update user model

--- backend/src/app/core/models/user.py
+++ backend/src/app/core/models/user.py
@@ -1,5 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
 from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
 
 
-class User(Base):
-    pass
+class User(IntIdPkMixin, Base):
+    username: Mapped[str] = mapped_column(unique=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    password: Mapped[str]
+    gender: Mapped[str]
+    age: Mapped[int]
