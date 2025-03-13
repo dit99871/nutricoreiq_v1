0633aa035ca1675122ee20d8c5e345fad281936d
di <dit99871@gmail.com>
2025-03-13 20:28:51 +0400

    Create gender_age_specific_nutrient schema

---
+++ backend/src/app/core/schemas/gender_age_specific_nutrient.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from .base import BaseSchema
+
+
+class GenderAgeSpecificNutrientsBase(BaseModel):
+    age_group_id: int
+    nutrient_id: int
+    demand: float
+
+
+class MaleAgeSpecificNutrientsCreate(GenderAgeSpecificNutrientsBase):
+    pass
+
+
+class MaleAgeSpecificNutrientsRead(GenderAgeSpecificNutrientsBase, BaseSchema):
+    id: int
+
+class FemaleAgeSpecificNutrientsCreate(GenderAgeSpecificNutrientsBase):
+    pass
+
+
+class FemaleAgeSpecificNutrientsRead(GenderAgeSpecificNutrientsBase, BaseSchema):
+    id: int
