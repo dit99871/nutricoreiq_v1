670db28520b36bd807e2c7900ec6492c30824ba0
di <dit99871@gmail.com>
2025-03-14 12:04:41 +0400

    Update imports

--- backend/src/app/core/models/__init__.py
+++ backend/src/app/core/models/__init__.py
@@ -1,27 +1,27 @@
-__all__ = (
-    "AgeGroup",
-    "db_helper",
-    "Base",
-    "FemaleAgeSpecificNutrient",
-    "MaleAgeSpecificNutrient",
-    "FemalePAL",
-    "MalePAL",
-    "FemaleMetabolism",
-    "MaleMetabolism",
-    "Nutrient",
-    "Product",
-    "User",
-)
-
-from .age_group import AgeGroup
-from .db_helper import db_helper
-from .base import Base
-from .gender_age_specific_nutrient import (
-    FemaleAgeSpecificNutrient,
-    MaleAgeSpecificNutrient,
-)
-from .gender_pal import FemalePAL, MalePAL
-from .gender_metabolism import FemaleMetabolism, MaleMetabolism
-from .nutrient import Nutrient
-from .product import Product
-from .user import User
+# __all__ = (
+#     "AgeGroup",
+#     "db_helper",
+#     "Base",
+#     "FemaleAgeSpecificNutrient",
+#     "MaleAgeSpecificNutrient",
+#     "FemalePAL",
+#     "MalePAL",
+#     "FemaleMetabolism",
+#     "MaleMetabolism",
+#     "Nutrient",
+#     "Product",
+#     "User",
+# )
+#
+# from .age_group import AgeGroup
+# from .db_helper import db_helper
+# from .base import Base
+# from .gender_age_specific_nutrient import (
+#     FemaleAgeSpecificNutrient,
+#     MaleAgeSpecificNutrient,
+# )
+# from .gender_pal import FemalePAL, MalePAL
+# from .gender_metabolism import FemaleMetabolism, MaleMetabolism
+# from .nutrient import Nutrient
+# from .product import Product
+# from .user import User
