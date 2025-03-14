2264d477a61d5589bcced60e9d9da61e651b386d
di <dit99871@gmail.com>
2025-03-14 08:44:49 +0400

    Update LoggingConfig

--- backend/src/app/core/config.py
+++ backend/src/app/core/config.py
@@ -28,6 +28,9 @@ class LoggingConfig(BaseModel):
         "critical",
     ] = "info"
     log_format: str = LOG_DEFAULT_FORMAT
+    log_file: str = "app.log"  # Путь к файлу логов
+    log_file_max_size: int = 5 * 1024 * 1024  # 5 MB
+    log_file_backup_count: int = 3  # Количество backup файлов
 
     @property
     def log_level_value(self) -> int:
