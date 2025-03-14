514f69129288dcbbd62c685a34286ac5d6d5972b
di <dit99871@gmail.com>
2025-03-14 08:52:37 +0400

    Update main by setup_logging()

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,14 +1,10 @@
-import logging
-
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
+from utils.logger import setup_logging
 
-logging.basicConfig(
-    level=settings.logging.log_level_value,
-    format=settings.logging.log_format,
-)
+setup_logging()
 
 app = FastAPI()
 
