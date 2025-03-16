4a324f17a738b60d1baed7b030e8178d2a93b56d
di <dit99871@gmail.com>
2025-03-16 20:31:39 +0400

    Create unit tests for user crud's

--- backend/src/app/crud/__init__.py
+++ backend/src/app/crud/__init__.py
@@ -4,6 +4,14 @@ __all__ = (
     "get_user",
     "get_user_by_email",
     "update_user",
+    "_get_user_by_filter",
 )
 
-from .user import create_user, delete_user, get_user, get_user_by_email, update_user
+from .user import (
+    create_user,
+    delete_user,
+    get_user,
+    get_user_by_email,
+    update_user,
+    _get_user_by_filter,
+)
