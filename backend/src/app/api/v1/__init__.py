4208c3022169a37b2a3bfbc0212f00eb8e3f03ff
di <dit99871@gmail.com>
2025-03-17 21:12:14 +0400

    Update imports

--- backend/src/app/api/v1/__init__.py
+++ backend/src/app/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .auth import router as auth_router
 from core.config import settings
 from .user import router as users_router
 
@@ -9,4 +10,8 @@ router = APIRouter(
 router.include_router(
     users_router,
     prefix=settings.api.v1.user,
+)
+router.include_router(
+    auth_router,
+    prefix=settings.api.v1.auth,
 )
