e95c06aa0d0ecb1bb71a7dfed04cd225be72b008
di <dit99871@gmail.com>
2025-03-18 20:03:26 +0400

    Update imports

--- backend/src/app/crud/__init__.py
+++ backend/src/app/crud/__init__.py
@@ -1,8 +1,8 @@
 __all__ = (
     "create_user",
     "delete_user",
-    "get_user",
     "get_user_by_email",
+    "get_user_by_name",
     "update_user",
     "_get_user_by_filter",
 )
@@ -10,8 +10,8 @@ __all__ = (
 from .user import (
     create_user,
     delete_user,
-    get_user,
     get_user_by_email,
+    get_user_by_name,
     update_user,
     _get_user_by_filter,
 )
