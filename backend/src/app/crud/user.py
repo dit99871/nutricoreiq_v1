304b65410aa3d7659aeb4355f4a76920a5ce8983
di <dit99871@gmail.com>
2025-03-19 19:53:24 +0400

    Update user crud

--- backend/src/app/crud/user.py
+++ backend/src/app/crud/user.py
@@ -6,10 +6,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.exc import SQLAlchemyError
 
-from core.utils import db_helper, get_password_hash
+from core.utils import db_helper, get_password_hash, log_user_result
 from core.logger import get_logger
-from models import DeletedUser, User
-from schemas import UserCreate, UserDelete, UserUpdate
+from models import User
+from schemas import UserCreate  # UserDelete, UserUpdate
 
 # Настройка логирования
 log = get_logger(__name__)
@@ -33,40 +33,15 @@ async def _get_user_by_filter(
         raise  # Пробрасываем исключение
 
 
-async def _log_user_result(
-    user: Optional[User],
-    success_message: str,
-    not_found_message: str,
-) -> Optional[User]:
-    """Обрабатывает результат запроса пользователя и логирует сообщение."""
-    if user:
-        log.info(success_message)
-    else:
-        log.warning(not_found_message)
-    return user
-
-
-async def get_user_by_id(
-    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    user_id: int,
-) -> Optional[User]:
-    """Получение пользователя по ID."""
-    user = await _get_user_by_filter(db, User.id == user_id)
-    return await _log_user_result(
-        user,
-        f"User found with id: {user_id}",
-        f"User not found with id: {user_id}",
-    )
-
-
 async def get_user_by_email(
     db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     email: EmailStr,
 ) -> Optional[User]:
     """Получение пользователя по email."""
     user = await _get_user_by_filter(db, User.email == email)
-    return await _log_user_result(
+    return await log_user_result(
         user,
+        log,
         f"User found with email: {email}",
         f"User not found with email: {email}",
     )
@@ -77,8 +52,9 @@ async def get_user_by_name(
     user_name: str,
 ) -> Optional[User]:
     user = await _get_user_by_filter(db, User.username == user_name)
-    return await _log_user_result(
+    return await log_user_result(
         user,
+        log,
         f"User found with user_name: {user_name}",
         f"User not found with user_name: {user_name}",
     )
@@ -114,69 +90,70 @@ async def create_user(
         return None
 
 
-async def update_user(
-    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    user_email: EmailStr,
-    user: UserUpdate,
-) -> Optional[User]:
-    """Обновление данных пользователя."""
-    try:
-        db_user = await get_user_by_email(db, user_email)
-        if db_user:
-            for key, value in user.model_dump().items():
-                if value is not None:
-                    setattr(db_user, key, value)
-            await db.commit()
-            await db.refresh(db_user)
-            log.info("User updated with email: %s", user_email)
-            return db_user
-        log.warning("User not found for update with email: %s", user_email)
-        return None
-    except SQLAlchemyError as e:
-        log.error("Database error updating user with email %s: %s", user_email, e)
-        await db.rollback()
-        return None
-    except Exception as e:
-        log.exception("Unexpected error updating user with email %s: %s", user_email, e)
-        await db.rollback()
-        return None
-
-
-async def delete_user(
-    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    user_email: EmailStr,
-    user: UserDelete,
-) -> Optional[DeletedUser]:
-    """Удаление пользователя."""
-    try:
-        db_user = await get_user_by_email(db, user_email)
-        if db_user:
-            # Создаем запись в таблице deleted_users
-            deleted_user = DeletedUser(
-                username=db_user.username,
-                email=db_user.email,
-                hashed_password=db_user.hashed_password,
-                gender=db_user.gender,
-                age=db_user.age,
-                weight=db_user.weight,
-                is_active=db_user.is_active,
-            )
-            db.add(deleted_user)
-            await db.commit()
-            await db.refresh(deleted_user)
-
-            # Удаляем пользователя из таблицы users
-            await db.delete(db_user)
-            await db.commit()
-            log.info("User deleted with email: %s", user_email)
-            return deleted_user
-        log.warning("User not found for deletion with email: %s", user_email)
-        return None
-    except SQLAlchemyError as e:
-        log.error("Database error deleting user with email %s: %s", user_email, e)
-        await db.rollback()
-        return None
-    except Exception as e:
-        log.exception("Unexpected error deleting user with email %s: %s", user_email, e)
-        await db.rollback()
-        return None
+#
+# async def update_user(
+#     db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+#     user_email: EmailStr,
+#     user: UserUpdate,
+# ) -> Optional[User]:
+#     """Обновление данных пользователя."""
+#     try:
+#         db_user = await get_user_by_email(db, user_email)
+#         if db_user:
+#             for key, value in user.model_dump().items():
+#                 if value is not None:
+#                     setattr(db_user, key, value)
+#             await db.commit()
+#             await db.refresh(db_user)
+#             log.info("User updated with email: %s", user_email)
+#             return db_user
+#         log.warning("User not found for update with email: %s", user_email)
+#         return None
+#     except SQLAlchemyError as e:
+#         log.error("Database error updating user with email %s: %s", user_email, e)
+#         await db.rollback()
+#         return None
+#     except Exception as e:
+#         log.exception("Unexpected error updating user with email %s: %s", user_email, e)
+#         await db.rollback()
+#         return None
+#
+#
+# async def delete_user(
+#     db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+#     user_email: EmailStr,
+#     user: UserDelete,
+# ) -> Optional[DeletedUser]:
+#     """Удаление пользователя."""
+#     try:
+#         db_user = await get_user_by_email(db, user_email)
+#         if db_user:
+#             # Создаем запись в таблице deleted_users
+#             deleted_user = DeletedUser(
+#                 username=db_user.username,
+#                 email=db_user.email,
+#                 hashed_password=db_user.hashed_password,
+#                 gender=db_user.gender,
+#                 age=db_user.age,
+#                 weight=db_user.weight,
+#                 is_active=db_user.is_active,
+#             )
+#             db.add(deleted_user)
+#             await db.commit()
+#             await db.refresh(deleted_user)
+#
+#             # Удаляем пользователя из таблицы users
+#             await db.delete(db_user)
+#             await db.commit()
+#             log.info("User deleted with email: %s", user_email)
+#             return deleted_user
+#         log.warning("User not found for deletion with email: %s", user_email)
+#         return None
+#     except SQLAlchemyError as e:
+#         log.error("Database error deleting user with email %s: %s", user_email, e)
+#         await db.rollback()
+#         return None
+#     except Exception as e:
+#         log.exception("Unexpected error deleting user with email %s: %s", user_email, e)
+#         await db.rollback()
+#         return None
