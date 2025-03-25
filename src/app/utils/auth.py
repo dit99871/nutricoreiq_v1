fd351f2654d773af7f22d4ff47131c14967590bf
di <dit99871@gmail.com>
2025-03-25 12:17:33 +0400

    Replace type to param

--- src/app/utils/auth.py
+++ src/app/utils/auth.py
@@ -13,7 +13,7 @@ from core.logger import get_logger
 log = get_logger(__name__)
 
 
-def get_password_hash(password: bytes) -> bytes:
+def get_password_hash(password: str) -> bytes:
     """
     Returns bytes object of hashed password.
 
@@ -23,7 +23,7 @@ def get_password_hash(password: bytes) -> bytes:
     :return: Hashed password as bytes.
     """
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password, salt)
+    return bcrypt.hashpw(password.encode(), salt)
 
 
 def verify_password(
