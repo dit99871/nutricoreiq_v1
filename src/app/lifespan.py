5ce0096a072e146b80f83d2e455acc6e3f2cbf54
di <dit99871@gmail.com>
2025-03-26 15:39:26 +0400

    Replace lifespan

---
+++ src/app/lifespan.py
@@ -0,0 +1,10 @@
+from contextlib import asynccontextmanager, contextmanager
+import subprocess
+
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def docker_lifespan(app: FastAPI):
+    subprocess.run(["docker-compose", "up", "-d"], check=True)
+    yield
