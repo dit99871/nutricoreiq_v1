b361a8ac5b56f1a2e63f57b0aef12b0d16cdaa77
di <dit99871@gmail.com>
2025-03-27 20:43:56 +0400

    Create generate_csp_nonse

--- src/app/utils/security.py
+++ src/app/utils/security.py
@@ -1,5 +1,9 @@
-from secrets import token_hex
+from secrets import token_hex, token_urlsafe
 
 
 def generate_csrf_token() -> str:
     return token_hex(32)
+
+
+def generate_csp_nonce() -> str:
+    return token_urlsafe(32)
