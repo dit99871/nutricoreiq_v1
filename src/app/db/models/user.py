3b1de8b023d2b7bffd0b45442fb9f50d7f5df105
di <dit99871@gmail.com>
2025-03-27 20:44:13 +0400

    Update User model

--- src/app/db/models/user.py
+++ src/app/db/models/user.py
@@ -8,10 +8,10 @@ class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True, index=True)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[bytes]
-    gender: Mapped[str]
-    age: Mapped[int]
-    weight: Mapped[float]
-    height: Mapped[int]
+    gender: Mapped[str | None] = mapped_column(nullable=True)
+    age: Mapped[int | None] = mapped_column(nullable=True)
+    weight: Mapped[float | None] = mapped_column(nullable=True)
+    height: Mapped[int | None] = mapped_column(nullable=True)
     is_active: Mapped[bool] = mapped_column(default=True)
     role: Mapped[str] = mapped_column(default="user")
     created_at: Mapped[str]
