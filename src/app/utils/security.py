3dc0e4fe3122c0bc33f4099a508eb8254ea90535
di <dit99871@gmail.com>
2025-03-29 14:53:22 +0400

    Create gen_hash_refresh_token

--- src/app/utils/security.py
+++ src/app/utils/security.py
@@ -1,3 +1,4 @@
+import hashlib
 from secrets import token_hex, token_urlsafe
 
 
@@ -7,3 +8,7 @@ def generate_csrf_token() -> str:
 
 def generate_csp_nonce() -> str:
     return token_urlsafe(32)
+
+
+def gen_hash_refresh_token(token: str) -> str:
+    return hashlib.sha256(token.encode()).hexdigest()
