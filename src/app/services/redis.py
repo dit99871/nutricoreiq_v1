13c84d20c8e6fd1cdd17ce97f9caba68b9ae8a1d
di <dit99871@gmail.com>
2025-03-31 10:23:54 +0400

    Create func to set hashed token in redis

--- src/app/services/redis.py
+++ src/app/services/redis.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from redis.asyncio import Redis
 from fastapi import Depends
 
 from core.redis import get_redis
+from utils.security import generate_hash_token
 
 
 class RedisService:
@@ -16,3 +19,17 @@ class RedisService:
 
 async def get_redis_service(redis: Redis = Depends(get_redis)):
     return RedisService(redis)
+
+
+async def set_in_redis(
+    user_id: int,
+    jwt: str,
+    exp: timedelta,
+    redis: Redis = Depends(get_redis),
+):
+    token_hash = generate_hash_token(jwt)
+    await redis.set(
+        f"refresh_token:{user_id}:{token_hash}",
+        "valid",
+        ex=exp,
+    )
