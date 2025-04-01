003db86874bf6e4d582c3673c9ee6e0ca30579f0
di <dit99871@gmail.com>
2025-04-01 20:49:54 +0400

    Update redis service by using async iterator to get redis client

--- src/app/services/redis.py
+++ src/app/services/redis.py
@@ -10,33 +10,20 @@ from utils.security import generate_hash_token
 log = get_logger("redis_service")
 
 
-class RedisService:
-    """ """
-
-    def __init__(self, redis: Redis):
-        self.redis = redis
-
-    async def get_user_session(self, user_id: str):
-        return await self.redis.get(f"user:{user_id}:session")
-
-
-async def get_redis_service(redis: Redis = Depends(get_redis)):
-    return RedisService(redis)
-
-
-async def set_in_redis(
-    user_id: int,
+async def add_refresh_to_redis(
+    uid: str,
     jwt: str,
     exp: dt.timedelta,
-    redis: Redis = Depends(get_redis),
 ):
     try:
-        token_hash = generate_hash_token(jwt)
-        await redis.set(
-            f"refresh_token:{user_id}:{token_hash}",
-            "valid",
-            ex=exp,
-        )
+        async for redis in get_redis():
+            token_hash = generate_hash_token(jwt)
+            await redis.set(
+                f"refresh_token:{uid}:{token_hash}",
+                "valid",
+                ex=exp,
+            )
+            log.info("Refresh token added to redis")
     except RedisError as e:
         log.error("Redis error adding refresh token: %s", e)
         raise HTTPException(
