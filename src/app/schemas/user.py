597f9411dfd9ad6e02708ec4f21e293d17247a30
di <dit99871@gmail.com>
2025-04-02 20:15:23 +0400

    Update UserBase schema

--- src/app/schemas/user.py
+++ src/app/schemas/user.py
@@ -1,35 +1,38 @@
-from typing import Annotated, Literal
-from annotated_types import MaxLen, MinLen
-from pydantic import EmailStr, ConfigDict, field_validator
+from annotated_types import MinLen, MaxLen
+from pydantic import ConfigDict, EmailStr, Field
+from typing import Annotated, Literal, Optional
 
 from .base import BaseSchema
 
 
 class UserBase(BaseSchema):
-    username: Annotated[str, MinLen(3), MaxLen(20)]
+    username: str = Field(..., min_length=3, max_length=20)
     email: EmailStr
+    hashed_password: Optional[bytes]
+    gender: Optional[str] = Field(pattern="^(female|male)?$")
+    age: Optional[int]
+    weight: Optional[float]
+    height: Optional[float]
+    created_at: str
 
 
 class UserCreate(UserBase):
     password: Annotated[str, MinLen(8)]
 
 
-class UserResponse(UserBase):
+class UserResponse(BaseSchema):
     id: int
     uid: str
+    username: Annotated[str, MinLen(3), MaxLen(20)]
+    email: EmailStr
     hashed_password: bytes | None = None
 
 
 class UserProfile(BaseSchema):
     gender: Literal["female", "male"]
-    age: int
-    weight: float | int
-    height: float | int
-
-    @field_validator("age", "height")
-    def validate_positive_numbers(cls, v):
-        if v <= 0:
-            raise ValueError("Must be positive")
-        return v
+    age: int = Field(gt=0)
+    weight: float = Field(gt=0)
+    height: float = Field(gt=0)
+    created_at: str
 
     model_config = ConfigDict(strict=True)
