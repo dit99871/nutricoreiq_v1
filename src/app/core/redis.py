89207fc99b9353d4c902466586c74ea31d3d9178
di <dit99871@gmail.com>
2025-04-07 14:31:37 +0400

    Add docstrings to redis services

--- src/app/core/redis.py
+++ src/app/core/redis.py
@@ -1,4 +1,3 @@
-from contextlib import asynccontextmanager
 from typing import Any, AsyncGenerator
 
 from redis.asyncio import Redis
--- src/app/services/redis.py
+++ src/app/services/redis.py
@@ -2,7 +2,7 @@ import datetime as dt
 import time
 
 from redis.asyncio import RedisError, Redis
-from fastapi import HTTPException, status, Depends
+from fastapi import HTTPException, status
 
 from core.logger import get_logger
 from core.redis import get_redis
@@ -15,7 +15,20 @@ async def add_refresh_to_redis(
     uid: str,
     jwt: str,
     exp: dt.timedelta,
-):
+) -> None:
+    """
+    Adds a refresh token to the Redis database for a given user.
+
+    This function generates a hash of the provided JWT and stores it in Redis
+    with an expiration time. It ensures that no more than four tokens exist
+    for the user by deleting the oldest token if necessary. The token is stored
+    with a unique timestamp to maintain the order of creation.
+
+    :param uid: The user ID for which the refresh token is to be added.
+    :param jwt: The JSON Web Token to be added.
+    :param exp: The expiration duration for the token.
+    :raises HTTPException: If there is an error interacting with Redis.
+    """
     try:
         async for redis in get_redis():
             token_hash = generate_hash_token(jwt)
@@ -45,6 +58,22 @@ async def validate_refresh_jwt(
     refresh_token: str,
     redis: Redis,
 ) -> bool:
+    """
+    Validates a refresh token for a given user.
+
+    Validates a refresh token from the Redis database for a given user ID and
+    refresh token. The token is hashed and the corresponding key in Redis is
+    checked for existence. If the token is invalid, has expired, or does not
+    exist, raises an HTTPException with a 401 status code and an appropriate
+    error message.
+
+    :param uid: The user ID for which to validate the refresh token.
+    :param refresh_token: The refresh token to be validated.
+    :param redis: The Redis client to use for the query.
+    :raises HTTPException: If the token is invalid, has expired, or does not
+                           exist.
+    :return: True if the token is valid, False otherwise.
+    """
     try:
         token_hash = generate_hash_token(refresh_token)
         token_key = f"refresh_token:{uid}:{token_hash}:*"
@@ -60,6 +89,18 @@ async def revoke_refresh_token(
     refresh_token: str,
     redis: Redis,
 ) -> None:
+    """
+    Revokes a refresh token for a given user.
+
+    Revokes a refresh token from the Redis database for a given user ID and
+    refresh token. The token is hashed and the corresponding key in Redis is
+    deleted. The function logs a message when the token is successfully revoked.
+
+    :param uid: The user ID for which to revoke the refresh token.
+    :param refresh_token: The refresh token to be revoked.
+    :param redis: The Redis client to use for the query.
+    :return: None
+    """
     token_hash = generate_hash_token(refresh_token)
     token_key = f"refresh_token:{uid}:{token_hash}"
     await redis.delete(token_key)
@@ -69,7 +110,16 @@ async def revoke_refresh_token(
 async def revoke_all_refresh_tokens(
     uid: str,
 ) -> None:
-    # Находим все refresh токены пользователя и удаляем их
+    """
+    Revoke all refresh tokens for a given user.
+
+    This function revokes all refresh tokens for a given user id by deleting the
+    corresponding keys from Redis. If there are any Redis errors during the process,
+    it raises an HTTPException with a 401 status code.
+
+    :param uid: The user id for which to revoke all refresh tokens.
+    :type uid: str
+    """
     try:
         async for redis in get_redis():
             keys = await redis.keys(f"refresh_token:{uid}:*")
