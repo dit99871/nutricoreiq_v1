fa5ff6e185c2c739c4568d9904add7d68b1ac42f
di <dit99871@gmail.com>
2025-04-26 19:47:40 +0400

    Update Product model by search_vector

--- src/app/db/models/product.py
+++ src/app/db/models/product.py
@@ -1,4 +1,5 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Index, event, DDL
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
@@ -9,7 +10,20 @@ class Product(IntIdPkMixin, Base):
     title: Mapped[str] = mapped_column(nullable=False)
     group_id: Mapped[int] = mapped_column(ForeignKey("product_groups.id"))
 
+    search_vector: Mapped[TSVECTOR] = mapped_column(TSVECTOR())
+
     product_groups: Mapped["ProductGroup"] = relationship(back_populates="products")
     nutrient_associations: Mapped[list["ProductNutrient"]] = relationship(
         back_populates="products"
+    )
+
+    # Индексы
+    __table_args__ = (
+        Index("idx_product_search_vector", search_vector, postgresql_using="gin"),
+        Index(
+            "idx_product_title_trgm",
+            title,
+            postgresql_using="gin",
+            postgresql_ops={"title": "gin_trgm_ops"},
+        ),
     )
