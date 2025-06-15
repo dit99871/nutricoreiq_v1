cc2af4187e5d3d3496eaf9e3d36a3a232bf2c3c4
di <dit99871@gmail.com>
2025-06-15 20:36:24 +0400

    Добавление в ревизию алембика op.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm;")

--- src/app/alembic/versions/2025_05_15_0942-12d9ec7a4890_init_all_models.py
+++ src/app/alembic/versions/2025_05_15_0942-12d9ec7a4890_init_all_models.py
@@ -20,6 +20,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
+    op.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm;")
+
     op.create_table(
         "nutrients",
         sa.Column("id", sa.Integer(), nullable=False),
