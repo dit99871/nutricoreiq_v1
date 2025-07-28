6073fb458e5611e32310126766a4a84859019021
di <dit99871@gmail.com>
2025-07-28 08:32:02 +0400

    Подключение csp мидлваря

--- src/app/main.py
+++ src/app/main.py
@@ -4,9 +4,9 @@ import os
 from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse, ORJSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from prometheus_fastapi_instrumentator import Instrumentator
+# from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 from src.app.api import router as api_router
@@ -17,17 +17,18 @@ from src.app.core.exception_handlers import (
     validation_exception_handler,
 )
 from src.app.core.logger import setup_logging
+from src.app.core.middleware.csp_middleware import CSPMiddleware
 from src.app.core.middleware.csrf_middleware import CSRFMiddleware
 from src.app.core.middleware.redis_session_middleware import RedisSessionMiddleware
 from src.app.lifespan import lifespan
 from src.app.services.auth import get_current_auth_user
-from src.app.utils.security import generate_csp_nonce
 from src.app.utils.templates import templates
 
 setup_logging()
 
-app = FastAPI()
-Instrumentator().instrument(app).expose(app, endpoint="/metrics")
+app = FastAPI(lifespan=lifespan)
+
+# Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
 base_dir = os.path.dirname(os.path.dirname(__file__))
 static_dir = os.path.join(base_dir, "app", "static")
@@ -42,6 +43,8 @@ app.add_middleware(
     allow_headers=settings.cors.allow_headers,
     max_age=600,
 )
+
+app.add_middleware(CSPMiddleware)
 app.add_middleware(CSRFMiddleware)
 app.add_middleware(RedisSessionMiddleware)
 
@@ -69,7 +72,15 @@ def start_page(
             "current_year": datetime.now().year,
             "user": current_user,
             "csrf_token": redis_session.get("csrf_token"),
-            "csp_nonce": generate_csp_nonce(),
+            "csp_nonce": request.state.csp_nonce,
         },
     )
 
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "src.app.main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
