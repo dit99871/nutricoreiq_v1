fdb7e06b8062055500b4b40967bc32dd1ffe53b3
di <dit99871@gmail.com>
2025-07-28 08:28:47 +0400

    Применение secureFetch для логаута

--- src/app/static/js/scripts.js
+++ src/app/static/js/scripts.js
@@ -721,8 +721,8 @@ document.addEventListener("DOMContentLoaded", () => {
         if (logoutBtn) {
             logoutBtn.addEventListener('click', async () => {
                 try {
-                    await fetch('/api/v1/auth/logout', {
-                        method: 'GET',
+                    await secureFetch('/api/v1/auth/logout', {
+                        method: 'POST',
                         credentials: 'include',
                     });
                     showSuccess('Вы успешно вышли из аккаунта!');
