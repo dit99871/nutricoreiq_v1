2d48585730fcdff4d935a9447a479be0e43043bc
di <dit99871@gmail.com>
2025-07-29 16:11:55 +0400

    Добавление брокера в lifespan

--- src/app/lifespan.py
+++ src/app/lifespan.py
@@ -3,8 +3,10 @@ import subprocess
 
 from fastapi import FastAPI
 
-from src.app.core.redis import init_redis, close_redis
 from src.app.core.logger import get_logger
+from src.app.core.redis import init_redis, close_redis
+from src.app.core import broker
+from src.app.db import db_helper
 
 log = get_logger("lifespan")
 
@@ -15,8 +17,11 @@ async def lifespan(app: FastAPI):
         ["docker-compose", "-f", "docker-compose.dev.yml", "up", "-d"],
         check=True,
     )
+    await broker.startup()
     await init_redis()
     try:
         yield
     finally:
         await close_redis()
+        await broker.shutdown()
+        await db_helper.dispose()
