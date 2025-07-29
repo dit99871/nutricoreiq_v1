245185315faf27271d2bb4a09901a5b0cb7dd705
di <dit99871@gmail.com>
2025-07-29 16:17:20 +0400

    Добавление логики отправки приветственного письма при регистрации

--- src/app/api/v1/auth.py
+++ src/app/api/v1/auth.py
@@ -7,7 +7,7 @@ from fastapi import (
     status,
     Request,
 )
-from fastapi.responses import ORJSONResponse, RedirectResponse
+from fastapi.responses import ORJSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -26,6 +26,7 @@ from src.app.services.auth import (
     get_current_auth_user_for_refresh,
     authenticate_user,
 )
+from src.app.services.email import send_welcome_email
 from src.app.services.redis import revoke_refresh_token
 from src.app.utils.auth import create_response
 
@@ -47,26 +48,22 @@ async def register_user(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> UserCreate | None:
     """
-    Registers a new user in the system.
+    Registers a new user in the database.
 
-    This endpoint accepts a `UserCreate` object containing user registration
-    details and creates a new user account if the email is not already registered.
-    If the user is successfully created, it returns the created `UserCreate`
-    object. If the email is already registered, it raises a 400 HTTP exception.
-    In case of a database error, it raises a 500 HTTP exception.
+    Given a valid `UserCreate` object, registers a new user in the database.
+    If the user is already registered, raises an `HTTPException` with a 400
+    status code and a detail string containing the error message.
 
-    :param user_in: The user data for registration.
-    :param session: The current database session.
-    :return: The created `UserCreate` object or raises an HTTP exception.
-    :raises HTTPException: If the email is already registered or if a database error occurs.
+    :param user_in: The user data to register.
+    :param session: The database session to use for the query.
+    :return: The registered user object.
+    :raises HTTPException: If the user is already registered.
     """
     # log.info("Attempting to register user with email: %s", user_in.email)
     db_user = await get_user_by_email(session, user_in.email)
 
     if db_user:
-        log.warning(
-            "Registration failed: Email already registered: %s", user_in.email
-        )
+        log.warning("Registration failed: Email already registered: %s", user_in.email)
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={
@@ -75,6 +72,9 @@ async def register_user(
         )
     user = await create_user(session, user_in)
     log.info("User registered successfully: %s", user.email)
+
+    await send_welcome_email(user_in.email)
+
     return user
 
 
@@ -210,7 +210,6 @@ async def change_password(
     user: Annotated[UserResponse, Depends(get_current_auth_user)],
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
-
     """
     Changes the password for the authenticated user.
 
