8617c5feeeeeff790a4ab94e0146bb3c76c36a5d
di <dit99871@gmail.com>
2025-07-30 17:19:06 +0400

    Добавление в lifespan инициализации редиса, реббита + деинициализации редиса, БД

--- src/app/lifespan.py
+++ src/app/lifespan.py
@@ -1,27 +1,23 @@
 from contextlib import asynccontextmanager
-import subprocess
 
 from fastapi import FastAPI
 
-from src.app.core.logger import get_logger
 from src.app.core.redis import init_redis, close_redis
 from src.app.core import broker
 from src.app.db import db_helper
 
-log = get_logger("lifespan")
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    subprocess.run(
-        ["docker-compose", "-f", "docker-compose.dev.yml", "up", "-d"],
-        check=True,
-    )
-    await broker.startup()
     await init_redis()
+
+    if not broker.is_worker_process:
+        await broker.startup()
+
     try:
         yield
     finally:
         await close_redis()
-        await broker.shutdown()
         await db_helper.dispose()
+        if not broker.is_worker_process:
+            await broker.shutdown()
