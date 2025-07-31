7ca464c14325c62e93a84c6eedc555b5bc4d5fcf
di <dit99871@gmail.com>
2025-07-31 16:23:30 +0400

    Редактирование lifespan для загрузки брокера

--- src/app/lifespan.py
+++ src/app/lifespan.py
@@ -1,10 +1,23 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from tenacity import retry, stop_after_attempt, wait_fixed
 
-from src.app.core.redis import init_redis, close_redis
 from src.app.core import broker
+from src.app.core.logger import get_logger
+from src.app.core.redis import init_redis, close_redis
 from src.app.db import db_helper
+
+log = get_logger("lifespan")
+
+
+@retry(stop=stop_after_attempt(13), wait=wait_fixed(4))
+async def check_rabbitmq():
+    try:
+        await broker.startup()
+    except Exception as e:
+        log.error(f"RabbitMQ not ready: {e}")
+        raise
 
 
 @asynccontextmanager
@@ -12,8 +25,7 @@ async def lifespan(app: FastAPI):
     await init_redis()
 
     if not broker.is_worker_process:
-        await broker.startup()
-
+        await check_rabbitmq()
     try:
         yield
     finally:
