6640fe00c40ef21c8687e777d720935b78adb1f9
di <dit99871@gmail.com>
2025-08-02 06:13:52 +0400

    Редактирование настроек логгера

--- src/app/core/logger.py
+++ src/app/core/logger.py
@@ -14,6 +14,11 @@ def setup_logging() -> None:
     log_dir = Path(settings.logging.log_file).parent
     log_dir.mkdir(parents=True, exist_ok=True)
 
+    formatter = logging.Formatter(
+        fmt=settings.logging.log_format,
+        datefmt=settings.logging.log_date_format,
+    )
+
     # Хэндлер для записи в файл с ротацией
     file_handler = RotatingFileHandler(
         settings.logging.log_file,
@@ -21,12 +26,16 @@ def setup_logging() -> None:
         backupCount=settings.logging.log_file_backup_count,
     )
 
+    file_handler.setFormatter(formatter)
+
+    # Хэндлер для вывода в консоль
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
     # Настройка корневого логгера
     logging.basicConfig(
         level=settings.logging.log_level_value,
-        format=settings.logging.log_format,
-        datefmt=settings.logging.log_date_format,
-        handlers=[file_handler],
+        handlers=[file_handler, console_handler],
     )
 
 
