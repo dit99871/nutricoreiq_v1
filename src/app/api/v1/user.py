0764c676f80da910725cada6655bdb652dff5b17
di <dit99871@gmail.com>
2025-08-05 16:11:43 +0400

    Создание ручек для подписки и отписки email-сообщений

--- src/app/api/v1/user.py
+++ src/app/api/v1/user.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.app.core.logger import get_logger
 from src.app.crud.profile import update_user_profile, get_user_profile
-from src.app.crud.user import unsubscribe_email
+from src.app.crud.user import choose_subscribe_status
 from src.app.db import db_helper
 from src.app.schemas.user import UserProfile, UserResponse
 from src.app.services.auth import get_current_auth_user
@@ -138,4 +138,42 @@ async def unsubscribe_email_notification(
     user: Annotated[UserResponse, Depends(get_current_auth_user)],
     db_session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
-    await unsubscribe_email(user, db_session)
+    """
+    Unsubscribes the current authenticated user from email notifications.
+
+    This endpoint accepts the current authenticated user object and the
+    current database session. It then calls the `choose_subscribe_status`
+    function with the user object and the database session, along with the
+    boolean value `False` to indicate that the user wants to unsubscribe from
+    email notifications. If the user is not authenticated, it raises an
+    HTTPException with a 401 status code.
+
+    :param user: The authenticated user object obtained from the dependency.
+    :param db_session: The current database db_session.
+    :return: A JSON response indicating the success of the unsubscription.
+    :raises HTTPException: If the user is not authenticated.
+    """
+    await choose_subscribe_status(user, db_session, False)
+
+
+@router.post("/subscribe")
+async def subscribe_email_notification(
+    user: Annotated[UserResponse, Depends(get_current_auth_user)],
+    db_session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    """
+    Subscribes the current authenticated user to email notifications.
+
+    This endpoint accepts the current authenticated user object and the
+    current database session. It then calls the `choose_subscribe_status`
+    function with the user object and the database session, along with the
+    boolean value `True` to indicate that the user wants to subscribe to
+    email notifications. If the user is not authenticated, it raises an
+    HTTPException with a 401 status code.
+
+    :param user: The authenticated user object obtained from the dependency.
+    :param db_session: The current database db_session.
+    :return: A JSON response indicating the success of the subscription.
+    :raises HTTPException: If the user is not authenticated.
+    """
+    await choose_subscribe_status(user, db_session, True)
