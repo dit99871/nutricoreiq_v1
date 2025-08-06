a9772c673e6ad8db2f960b250b6aa5cd2729330f
di <dit99871@gmail.com>
2025-08-06 13:25:09 +0400

    Редактирование кастомного ExpiredTokenException

--- src/app/core/exceptions.py
+++ src/app/core/exceptions.py
@@ -1,6 +1,3 @@
-""" """
-
-from sqlalchemy.exc import SQLAlchemyError
 from fastapi import HTTPException, status
 
 
@@ -9,33 +6,12 @@ class ExpiredTokenException(HTTPException):
     Исключение для случаев, когда токен истек
     """
 
-    def __init__(self, detail: str = "Token has expired"):
+    def __init__(
+        self,
+        detail: str = "Срок действия токена истек. Пожалуйста, войдите заново.",
+    ):
 
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=detail,
         )
-
-
-class AppError(Exception):
-    """Базовое исключение для приложения."""
-
-    pass
-
-
-class NotFoundError(AppError):
-    """Исключение для случаев, когда данные не найдены."""
-
-    pass
-
-
-class ValidationError(AppError):
-    """Исключение для ошибок валидации."""
-
-    pass
-
-
-class DatabaseError(SQLAlchemyError, AppError):
-    """Исключение для ошибок базы данных."""
-
-    pass
